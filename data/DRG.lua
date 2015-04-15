61280433e95d1e40ee4b8f95ea7017aa3dd1abca
Kasonuga <gsgit@shackleford.33mail.com>
2015-04-15 05:59:16 -0400

    file rename

---
+++ data/DRG.lua
@@ -0,0 +1,168 @@
+-------------------------------------------------------------------------------------------------------------------
+-- Setup functions for this job.  Generally should not be modified.
+-------------------------------------------------------------------------------------------------------------------
+
+-- Initialization function for this job file.
+function get_sets()
+    mote_include_version = 2
+
+	-- Load and initialize the include file.
+	include('Mote-Include.lua')
+end
+
+
+-- Setup vars that are user-independent.  state.Buff vars initialized here will automatically be tracked.
+function job_setup()
+
+end
+
+
+-------------------------------------------------------------------------------------------------------------------
+-- User setup functions for this job.  Recommend that these be overridden in a sidecar file.
+-------------------------------------------------------------------------------------------------------------------
+
+-- Setup vars that are user-dependent.  Can override this function in a sidecar file.
+function user_setup()
+  	state.OffenseMode:options('Normal', 'Acc')
+	state.RangedMode:options('Normal')
+	state.HybridMode:options('Normal', 'Acc')
+	state.WeaponskillMode:options('Normal')
+	state.CastingMode:options('Normal')
+	state.IdleMode:options('Normal', 'PDT', 'MDT')
+	state.RestingMode:options('Normal')
+	state.PhysicalDefenseMode:options('PDT')
+	state.MagicalDefenseMode:options('MDT')
+
+    -- Additional local binds
+    send_command('bind ^` input /ja "Hasso" <me>')
+    send_command('bind !` input /ja "Seigan" <me>')
+	
+end
+
+
+-- Called when this job file is unloaded (eg: job change)
+function user_unload()
+    send_command('unbind ^`')
+    send_command('unbind !`')
+end
+
+function init_gear_sets()
+	
+	--------------------------------------
+	-- Precast sets
+	--------------------------------------
+	
+	-- Sets to apply to arbitrary JAs
+    sets.precast.JA['Provoke']  = { 
+    head="Hydra Salade", body="Hydra Haubert", hands="Valor Gauntlets", lring="Mermaid's Ring",
+    rring="Hercules' Ring", back="Lamia Mantle +1", waist="Warwolf Belt" }
+	
+	-- Sets to apply to any actions of spell.type
+	sets.precast.Waltz = {
+  	ammo="Bibiki Seashell", waist="Warwolf Belt" }
+		
+	-- Sets for specific actions within spell.type
+	sets.precast.Waltz['Healing Waltz'] = {}
+
+    -- Sets for fast cast gear for spells
+	sets.precast.FC = {ear2="Loquacious Earring"}
+
+    -- Fast cast gear for specific spells or spell maps
+	sets.precast.FC.Utsusemi = { }
+
+	-- Weaponskill sets
+	sets.precast.WS = {
+  	ammo="Tiphia Sting", head="Hecatomb Cap +1", neck=gear.ElementalGorget, lear="Tmph. Earring +1",
+  	rear="Brutal Earring", body="Hct. Harness +1", hands="Hct. Mittens +1", ring1="Triumph Ring +1", ring2="Rajas Ring",
+  	back="Cerb. Mantle +1", waist="Warwolf Belt", legs="Hct. Subligar +1", feet="Hct. Leggings +1" }
+
+	-- Specific weaponskill sets.
+
+	
+	--------------------------------------
+	-- Midcast sets
+	--------------------------------------
+
+    -- Generic spell recast set
+	sets.midcast.FastRecast = {
+   	head="Walahra Turban", hands="Dusk Gloves +1", waist="Velocious Belt",
+   	legs="Byakko's Haidate", feet="Dusk Ledelsens +1", ring1="Blitz ring" }
+		
+	-- Specific spells
+	sets.midcast.Utsusemi = {}
+
+  	sets.midcast.Stoneskin = { body="Blood Scale Mail" }
+
+	
+	--------------------------------------
+	-- Idle/resting/defense/etc sets
+	--------------------------------------
+	
+	-- Resting sets
+	sets.resting = {
+    ammo="Bibiki Seashell", head="Coral Visor +1", neck="Orochi Nodowa +1", lear="Merman's Earring",
+    body="Conte Corazza", hands="Coral Fng. Gnt. +1", ring1="Merman's Ring", rear="Merman's Earring",
+    waist="Resolute Belt", ring2="Merman's Ring", back="Lamia Mantle +1", legs="Blood Cuisses",
+    feet="Askar Gambieras" }
+
+	
+	-- Idle sets
+	sets.idle = {
+   	ammo="Bibiki Seashell", head="Coral Visor +1", neck="Orochi Nodowa +1", lear="Merman's Earring",
+   	body="Conte Corazza", hands="Coral Fng. Gnt. +1", ring1="Merman's Ring", rear="Merman's Earring",
+   	waist="Resolute Belt", ring2="Merman's Ring", back="Lamia Mantle +1", legs="Blood Cuisses",
+   	feet="Askar Gambieras" }
+
+	sets.idle.Town = {
+    ammo="Bibiki Seashell", head="Coral Visor +1", neck="Orochi Nodowa +1", lear="Merman's Earring",
+    body="Conte Corazza", hands="Coral Fng. Gnt. +1", ring1="Merman's Ring", rear="Merman's Earring",
+    waist="Resolute Belt", ring2="Merman's Ring", back="Lamia Mantle +1", legs="Blood Cuisses",
+    feet="Askar Gambieras" }
+
+	
+	sets.idle.Weak = {
+    ammo="Bibiki Seashell", head="Coral Visor +1", neck="Orochi Nodowa +1", lear="Merman's Earring",
+    body="Conte Corazza", hands="Coral Fng. Gnt. +1", ring1="Merman's Ring", rear="Merman's Earring",
+    waist="Resolute Belt", ring2="Merman's Ring", back="Lamia Mantle +1", legs="Blood Cuisses",
+    feet="Askar Gambieras" }
+
+	
+	-- Defense sets
+	sets.defense.PDT = {
+   	ammo="Bibiki Seashell", head="Dst. Cap +1", neck="Orochi Nodowa +1", body="Dst. Harness +1",
+   	hands="Dst. Mittens +1", ring1="Jelly Ring", legs="Dst. Subligar +1", feet="Askar Gambieras" }
+
+	sets.defense.MDT = {
+   	head="Coral Visor +1", neck="Jeweled Collar +1", body="Cor. Scale Mail +1", hands="Coral Fng. Gnt. +1",
+   	ring1="Merman's Ring", ring2="Merman's Ring", waist="Resolute Belt", lear="Merman's Earring",
+   	back="Lamia Mantle +1", waist="Resolute Belt", rear="Merman's Earring", legs="Coral Cuisses +1",
+   	feet="Askar Gambieras" }
+
+    -- Gear to wear for kiting
+	sets.Kiting = { legs="Blood Cuisses" }
+
+	--------------------------------------
+	-- Engaged sets
+	--------------------------------------
+
+	-- Variations for TP weapon and (optional) offense/defense modes.  Code will fall back on previous
+	-- sets if more refined versions aren't defined.
+	-- If you create a set with both offense and defense modes, the offense mode should be first.
+	-- EG: sets.engaged.Dagger.Accuracy.Evasion
+	
+	-- Normal melee group
+	sets.engaged = {
+  	ammo="Tiphia Sting", head="Walahra Turban", neck="Ancient Torque", body="Homam Corazza",
+ 	hands="Dusk Gloves +1", waist="Velocious Belt", back="Cerb. Mantle +1",  legs="Aurum Cuisses",
+	feet="Dusk Ledelsens +1", lear="Assault Earring", rear="Brutal Earring", ring1="Blitz ring", ring2="Rajas Ring" }
+
+  	sets.engaged.Acc = {
+  	ammo="Tiphia Sting", head="Walahra Turban", neck="Ancient Torque", body="Homam Corazza",
+  	hands="Homam Manopolas", waist="Velocious Belt", back="Cerb. Mantle +1",  legs="Aurum Cuisses",
+  	feet="Aurum Sabatons", lear="Assault Earring", rear="Brutal Earring", ring1="Toreador's ring", ring2="Rajas Ring" }
+
+	--------------------------------------
+	-- Custom buff sets
+	--------------------------------------
+
+end
---
+++ data/SAM.lua
@@ -0,0 +1,175 @@
+-------------------------------------------------------------------------------------------------------------------
+-- Setup functions for this job.  Generally should not be modified.
+-------------------------------------------------------------------------------------------------------------------
+
+-- Initialization function for this job file.
+function get_sets()
+    mote_include_version = 2
+
+	-- Load and initialize the include file.
+	include('Mote-Include.lua')
+end
+
+-- Setup vars that are user-dependent.  Can override this function in a sidecar file.
+function user_setup()
+	state.OffenseMode:options('GK', 'PLM')
+	state.RangedMode:options('Normal')
+	state.HybridMode:options('Normal')
+	state.WeaponskillMode:options('Normal')
+	state.CastingMode:options('Normal')
+	state.IdleMode:options('Normal')
+	state.RestingMode:options('Normal')
+	state.PhysicalDefenseMode:options('PDT')
+	state.MagicalDefenseMode:options('MDT')
+
+
+    -- Additional local binds
+    send_command('bind ^` input /ja "Hasso" <me>')
+    send_command('bind !` input /ja "Seigan" <me>')
+
+end
+
+-- Called when this job file is unloaded (eg: job change)
+function user_unload()
+    send_command('unbind ^`')
+    send_command('unbind !`')
+end
+
+-- Setup vars that are user-independent.  state.Buff vars initialized here will automatically be tracked.
+function job_setup()
+
+end
+
+
+
+function init_gear_sets()	
+	--------------------------------------
+	-- Precast sets
+	--------------------------------------
+	
+	-- Sets to apply to arbitrary JAs
+	sets.precast.JA['Meditate'] = {
+	head="Myochin Kabuto", hands="Saotome Kote"}
+	
+	
+	-- Sets to apply to any actions of spell.type
+	sets.precast.Waltz = { ammo="Bibiki Seashell", head="Genbu's Kabuto", waist="Warwolf Belt" }
+		
+	-- Sets for specific actions within spell.type
+	sets.precast.Waltz['Healing Waltz'] = { }
+
+    -- Sets for fast cast gear for spells
+	sets.precast.FC = { rear="Loquacious Earring" }
+
+    -- Fast cast gear for specific spells or spell maps
+
+	-- Weaponskill sets
+--	sets.precast.WS = { 
+--	head="Shr.Znr.Kabuto +1", neck=gear.ElementalGorget, lear="Tmph. Earring +1", rear="Brutal Earring",
+--	body="Haubergeon +1", hands="Alkyoneus's Brc.", ring1="Triumph Ring +1", waist="Warwolf Belt",
+--	back="Cerb. Mantle +1", legs="Shura Haidate +1", feet="Rutter Sabatons" } 
+
+	sets.precast.WS = { -- pre 75
+	head="Voyager Sallet", neck="Spike Necklace", lear="Tmph. Earring +1", rear="Merman's Earring",
+	body="Byrnie +1", hands="Ochiudo's Kote", ring1="Victory Ring +1", waist="Potent Belt",
+	back="Amemet Mantle +1", legs="Myochin Haidate", feet="Rutter Sabatons" } 
+	
+	sets.precast.WS['Penta Thrust'] = set_combine( sets.precast.WS, { hands="Ochiudo's Kote" } )
+
+	-- Specific weaponskill sets.
+
+	
+	--------------------------------------
+	-- Midcast sets
+	--------------------------------------
+
+    -- Generic spell recast set
+	sets.midcast.FastRecast = {
+	Head="Walahra Turban", hands="Dusk Gloves +1", waist="Velocious Belt", legs="Byakko's Haidate", feet="Dusk Ledlesens +1" }
+		
+	-- Specific spells
+
+	--------------------------------------
+	-- Idle/resting/defense/etc sets
+	--------------------------------------
+	
+	-- Resting sets
+	sets.resting = {
+	ammo="Bibiki Seashell", head="Arh. Jinpachi +1", neck="Orochi Nodowa +1",  lear="Merman's Earring", rear="Merman's Earring", 
+	body="Arhat's Gi +1", hands="Dst. Mittens +1", ring1="Merman's Ring", ring2="Merman's Ring",
+	back="Lamia Mantle +1", waist="Resolute Belt", legs="Coral Cuisses +1", feet="Coral Greaves +1" }
+	
+
+	-- Idle sets
+	sets.idle = {
+	ammo="Bibiki Seashell", head="Arh. Jinpachi +1", neck="Orochi Nodowa +1",  lear="Merman's Earring", rear="Merman's Earring", 
+	body="Arhat's Gi +1", hands="Dst. Mittens +1", ring1="Merman's Ring", ring2="Merman's Ring",
+	back="Lamia Mantle +1", waist="Resolute Belt", legs="Coral Cuisses +1", feet="Coral Greaves +1" }
+
+	sets.idle.Town = {
+	ammo="Bibiki Seashell", head="Arh. Jinpachi +1", neck="Orochi Nodowa +1",  lear="Merman's Earring", rear="Merman's Earring", 
+	body="Arhat's Gi +1", hands="Dst. Mittens +1", ring1="Merman's Ring", ring2="Merman's Ring",
+	back="Lamia Mantle +1", waist="Resolute Belt", legs="Coral Cuisses +1", feet="Coral Greaves +1" }
+	
+	sets.idle.Weak = {
+	ammo="Bibiki Seashell", head="Arh. Jinpachi +1", neck="Orochi Nodowa +1",  lear="Merman's Earring", rear="Merman's Earring", 
+	body="Arhat's Gi +1", hands="Dst. Mittens +1", ring1="Merman's Ring", ring2="Merman's Ring",
+	back="Lamia Mantle +1", waist="Resolute Belt", legs="Coral Cuisses +1", feet="Coral Greaves +1" }
+	
+	-- Defense sets
+	sets.defense.PDT = {
+	ammo="Bibiki Seashell", head="Arh. Jinpachi +1", neck="Evasion Torque", body="Arhat's Gi +1",
+	hands="Dst. Mittens +1", ring1="Jelly Ring", legs="Dst. Subligar +1", feet="Askar Gambieras" }
+
+	sets.defense.MDT = {
+	head="Coral Visor +1", neck="Jeweled Collar +1", body="Cor. Scale Mail +1", hands="Coral Fng. Gnt. +1",
+	ring1="Merman's Ring", ring2="Merman's Ring", waist="Resolute Belt", lear="Merman's Earring", waist="Resolute Belt",
+	rear="Merman's Earring", back="Lamia Mantle +1", legs="Coral Cuisses +1", feet="Askar Gambieras" }
+
+    -- Gear to wear for kiting
+	sets.Kiting = {}
+
+	--------------------------------------
+	-- Engaged sets
+	--------------------------------------
+
+	-- Variations for TP weapon and (optional) offense/defense modes.  Code will fall back on previous
+	-- sets if more refined versions aren't defined.
+	-- If you create a set with both offense and defense modes, the offense mode should be first.
+	-- EG: sets.engaged.Dagger.Accuracy.Evasion
+	
+	-- Normal melee group
+	sets.engaged = {
+	ammo="Black Tathlum", Head="Walahra Turban", Neck="Acient Torque", lear="Merman's Earring", rear="Brutal Earring",
+	body="Haubergeon +1", hands="Dusk Gloves +1", ring1="Toreador's Ring", ring2="Rajas Ring",
+	back="Cerb. Mantle +1", waist="Velocious Belt", legs="Byakko's Haidate", feet="Dusk Ledlesens +1" }
+
+--	sets.engaged.GK = {
+--	ammo="Black Tathlum", Head="Walahra Turban", Neck="Acient Torque", lear="Merman's Earring", rear="Brutal Earring",
+--	body="Haubergeon +1", hands="Dusk Gloves +1", ring1="Toreador's Ring", ring2="Rajas Ring",
+--	back="Cerb. Mantle +1", waist="Velocious Belt", legs="Byakko's Haidate", feet="Dusk Ledlesens +1" }
+
+	sets.engaged.GK = { -- till 75 gears
+	ammo="Tiphia Sting", Head="Zeal Cap +1", Neck="Peacock Amulet", lear="Merman's Earring", rear="Merman's Earring",
+	body="Haubergeon +1", hands="Ochiudo's Kote", ring1="Toreador's Ring", ring2="Rajas Ring",
+	back="Amemet Mantle +1", waist="Velocious Belt", legs="Shm. Haidate", feet="Fuma Kyahan" }
+
+--	sets.engaged.PLM = {
+--	ammo="Black Tathlum", Head="Walahra Turban", Neck="Acient Torque", lear="Merman's Earring", rear="Brutal Earring",
+--	body="Haubergeon +1", hands="Dusk Gloves +1", ring1="Toreador's Ring", ring2="Rajas Ring",
+--	back="Cerb. Mantle +1", waist="Velocious Belt", legs="Byakko's Haidate", feet="Dusk Ledlesens +1" }
+
+	sets.engaged.PLM = { -- till 75 gears
+	ammo="Tiphia Sting", Head="Zeal Cap +1", Neck="Peacock Amulet", lear="Merman's Earring", rear="Merman's Earring",
+	body="Haubergeon +1", hands="Dusk Gloves +1", ring1="Toreador's Ring", ring2="Rajas Ring",
+	back="Jaguar Mantle", waist="Velocious Belt", legs="Shm. Haidate", feet="Fuma Kyahan" }
+
+
+	--------------------------------------
+	-- Custom buff sets
+	--------------------------------------
+end
+
+-------------------------------------------------------------------------------------------------------------------
+-- Utility functions specific to this job.
+-------------------------------------------------------------------------------------------------------------------
